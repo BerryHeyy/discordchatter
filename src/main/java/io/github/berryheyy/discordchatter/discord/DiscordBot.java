f64d4e7f60b428a0f0d983f7bb12291110dca842
BerryHeyy <berkaykaplanbk@hotmail.com>
2021-10-23 14:36:17 +0200

    Improved message

--- src/main/java/io/github/berryheyy/discordchatter/discord/DiscordBot.java
+++ src/main/java/io/github/berryheyy/discordchatter/discord/DiscordBot.java
@@ -7,11 +7,16 @@ import org.javacord.api.DiscordApiBuilder;
 import org.javacord.api.event.message.MessageCreateEvent;
 
 import io.github.berryheyy.discordchatter.DiscordChatter;
+import io.github.berryheyy.discordchatter.Reference;
 import io.github.berryheyy.discordchatter.config.DiscordChatterConfig;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.ChatType;
 import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextFormatting;
 import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.util.text.event.ClickEvent;
+import net.minecraft.util.text.event.HoverEvent;
 import net.minecraftforge.fml.event.server.FMLServerStartedEvent;
 
 public final class DiscordBot {
@@ -30,9 +35,20 @@ public final class DiscordBot {
     private static void onMessageCreate(MessageCreateEvent event)
     {
         if (event.getChannel().getId() != Long.parseLong(DiscordChatterConfig.channel_id_string.get())) return;
+        if (event.getMessageAuthor().isWebhook()) return;
+        if (event.getMessageAuthor().isBotUser()) return;
         
         DiscordChatter.LOGGER.info(event.getMessageContent());
-        server.getPlayerList().broadcastMessage(new TranslationTextComponent(event.getMessageContent()), ChatType.CHAT, UUID.randomUUID());
+
+        ITextComponent userframe = new TranslationTextComponent("<%s>", event.getMessageAuthor().getDisplayName()).withStyle((style) -> {
+            return style.withColor(TextFormatting.BLUE)
+            .withClickEvent(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, String.format("<@%s> ", event.getMessageAuthor().getIdAsString())))
+            .withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new TranslationTextComponent("Click to mention user.")));
+        });
+
+        ITextComponent finalMessage = new TranslationTextComponent("%s %s", userframe, event.getMessageContent());
+
+        server.getPlayerList().broadcastMessage(finalMessage, ChatType.CHAT, UUID.randomUUID());
     }
 
     
